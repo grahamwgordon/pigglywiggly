4ec3a0527e32a8b1bc8d0c4d4369488a293b3a63
grahamwgordon <grahamwalkergordon@gmail.com>
2020-08-13 22:12:35 -0700

    added mouse controls

--- Scenes/Environment.gd
+++ Scenes/Environment.gd
@@ -13,7 +13,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_key_pressed(KEY_SPACE):
-		self.set_cell(get_node("../Player").position.x / 16 + 1, get_node("../Player").position.y / 16, 0)
-	if Input.is_key_pressed(KEY_J):
-		self.set_cell(get_node("../Player").position.x / 16 + 1, get_node("../Player").position.y / 16, 1)
+	if Input.is_mouse_button_pressed(BUTTON_RIGHT):
+		self.set_cell(get_global_mouse_position().x / 16, get_global_mouse_position().y / 16, 0)
+	if Input.is_mouse_button_pressed(BUTTON_LEFT):
+		self.set_cell(get_global_mouse_position().x / 16, get_global_mouse_position().y / 16, 1)
